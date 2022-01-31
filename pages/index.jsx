17914153e69f685d1b07600d2ccb9c5fd557bcf5
miillaadddddd <milad7212@gmail.com>
2022-01-31 21:07:06 +0330

    Feat : Add footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,9 +5,21 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-cyan-500 flex justify-center items-center">
+    <div className="h-screen  flex justify-center items-center">
       <Button title="Confirm"/>
       <Button title="Reject" type="reject"/>
+
+      <footer>
+        <div className="fixed bottom-[-50px] left-0 right-0">
+          <Image
+            src="/wave.svg"
+            layout='responsive'
+            alt="logo"
+            width={4}
+            height={1}
+          />
+        </div>
+      </footer>
     </div>
   )
 }
