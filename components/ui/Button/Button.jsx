cd7e6f09cb956d6580c45c8e2a96fd91e78b7491
miillaadddddd <milad7212@gmail.com>
2022-01-31 20:07:17 +0330

    init : install tailwind

---
+++ components/ui/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button() {
+  return <> 
+  <button className=' p-40  bg-sky-50 rounded-md'>Milad</button>
+  </>;
+}
+
+export default Button;
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Button from '../components/ui/Button/Button'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-cyan-500 flex justify-center items-center">
+      <Button/>
+    </div>
+  )
+}
